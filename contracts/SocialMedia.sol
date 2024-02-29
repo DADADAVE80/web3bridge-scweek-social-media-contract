7f584f04dacf55339d35e5678258da0fe9b5dfa8
David Dada <daveproxy80@gmail.com>
2024-02-29 16:17:32 +0100

    logic updates

--- contracts/SocialMedia.sol
+++ contracts/SocialMedia.sol
@@ -9,31 +9,51 @@ contract SocialMedia is AccessControl {
         _grantRole(DEFAULT_ADMIN_ROLE, _admin);
     }
 
-    //
-    Account[] public accounts;
-    mapping(address => Account) accountMap;
-    mapping(address => bool) isRegistered;
-    mapping(uint256 => string) accountPosts;
+    modifier onlyAdmin(bytes32 _role) {
+        _checkRole(_role);
+        _;
+    }
 
-    //
+    // Account storage
+    Account[] public accounts;
+    mapping(address => Account) public accountMap;
+    mapping(address => bool) private isRegistered;
+
+    // Post storage
+    mapping(address => mapping(uint256 => string)) accountPosts;
+
+    // Sign up
     function signUp(string memory name, string memory username) public {
-        require(isRegistered[msg.sender] == false, "User has already been registered");
+        require(
+            isRegistered[msg.sender] == false,
+            "User has already been registered"
+        );
         Account user = new Account(name, username, msg.sender);
         accounts.push(user);
         accountMap[msg.sender] = user;
         isRegistered[msg.sender] = true;
     }
 
+    // Check profile
     function checkProfile() public view returns (Account) {
         return accountMap[msg.sender];
     }
 
-    //TODO: separate text post from multimedia post
+    // Create posts
+    // TODO: separate text post from multimedia post
     function createPost(string memory _content) public {
         require(isRegistered[msg.sender] == true, "You have to sign up first");
         Account user = accountMap[msg.sender];
-        uint256 postId = user.safeMint(msg.sender);
-        user.setTokenURI(_content);
-        accountPosts[postId] = _content;
+        uint256 postId = user.safeMint(msg.sender); //returns the id of minted NFT post
+        user.setTokenURI(_content); //sets the content of the post
+        accountPosts[msg.sender][postId] = _content;
+    }
+
+    // Share posts
+    function sharePost(
+        address _account,
+        uint256 _postId
+    ) external view returns (string memory) {
+        return accountPosts[_account][_postId];
     }
 }
